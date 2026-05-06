d0479708c34348c36e063b709c6ddecca44c0ca1
luisbrustolin <71842762+luisbrustolin@users.noreply.github.com>
2026-05-06 23:52:11 +0000

    #EDICAO#2

---
+++ my-app/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+
+  const a= 12;
+  return (
+    <>
+      <h1 className="text-center bg-red-500" >MEU SITE</h1>
+      <h2 className="text-center">LUIS CARLOS BRUSTOLIN</h2>
+      <HomeConent/>
+    </>
+  );
+}
+export  function HomeConent(){
+  return(
+    <h1>CLIQUE AQUI</h1>
+  )
+}
